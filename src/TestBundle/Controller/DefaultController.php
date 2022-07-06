8ad24c1e9f718edd7de323e31f969e95228b4619
desarrollo_infoexpo <desarrollo@infoexpo.com.mx>
2022-07-06 11:52:46 -0500

    Formulario productos y usuario  commit2

--- src/TestBundle/Controller/DefaultController.php
+++ src/TestBundle/Controller/DefaultController.php
@@ -10,4 +10,12 @@ class DefaultController extends Controller
     {
         return $this->render('TestBundle:Default:index.html.twig');
     }
+    public function formularioAction()
+    {
+        return $this->render('TestBundle:Default:formulario.html.twig');
+    }
+    public function formularioProductosAction()
+    {
+        return $this->render('TestBundle:Default:formularioProductos.html.twig');
+    }
 }
