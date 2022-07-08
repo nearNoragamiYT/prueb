7408baba1fc5e6c0098f71b0b24392879c1b20e6
nearNoragamiYT <108765433+nearNoragamiYT@users.noreply.github.com>
2022-07-08 09:26:48 -0500

    Revert "user2"

--- src/TestBundle/Controller/DefaultController.php
+++ src/TestBundle/Controller/DefaultController.php
@@ -34,10 +34,10 @@ class DefaultController extends Controller
     {
         $params = $request->request->all();
         $values = [
-            'name' => "'" . $params['nameP'] . "'",
+            'name' => "'". $params['nameP'] . "'",
             'active' => 'true'
         ];
-        $response = $this->defaultModel->insertProduct('public', $values, 0);
+        $response = $this->defaultModel->insertProduct('public',$values, 0);
         $message = 'No se inserto el producto';
         $response['message'] = $response['status'] ? 'OK' : $message;
         return new JsonResponse($response);
@@ -52,62 +52,4 @@ class DefaultController extends Controller
         $response['message'] = $response['status'] ? 'OK' : $message;
         return new JsonResponse($response);
     }
-
-    public function deleteProductAction(Request $request)
-    {
-        $params = $request->request->all();
-        $response = $this->defaultModel->deleteProduct($params);
-        $message = "No se pudo eliminar";
-        $response['message'] = $response['status'] ? 'se elimino con exito' : $message;
-        return new JsonResponse($response);
-    }
-
-
-
-
-
-
-
-
-    public function getUserAction(Int $id = 0)
-    {
-        $columns = ['id_user', 'name', 'active'];
-        $condition = ['active' => 'true'];
-        if ($id) $condition['id_user'] = $id;
-        $response = $this->defaultModel->getUsersOrUser('public', $columns, $condition);
-        $message = 'No hay usuarios';
-        $response['message'] = $response['data'] ? 'OK' : $message;
-        return new JsonResponse($response);
-    }
-
-    public function addUserAction(Request $request)
-    {
-        $params = $request->request->all();
-        $values = [
-            'name' => "'" . $params['nameU'] . "'",
-            'active' => 'true'
-        ];
-        $response = $this->defaultModel->insertUser('public', $values, 0);
-        $message = 'No se inserto el usuario';
-        $response['message'] = $response['status'] ? 'OK' : $message;
-        return new JsonResponse($response);
-    }
-
-    public function updateUsertAction(Request $request)
-    {
-        $params = $request->request->all();
-        $response = $this->defaultModel->updateUser($params);
-        $message = 'No se actualizo el usuario';
-        $response['message'] = $response['status'] ? 'OK' : $message;
-        return new JsonResponse($response);
-    }
-    public function deleteUserAction(Request $request)
-    {
-        $params = $request->request->all();
-        $response = $this->defaultModel->deleteUser($params);
-        $message = "No se pudo eliminar";
-        $response['message'] = $response['status'] ? 'se elimino con exito' : $message;
-        return new JsonResponse($response);
-    }
-
 }
--- src/TestBundle/Controller/DefaultModel.php
+++ src/TestBundle/Controller/DefaultModel.php
@@ -27,8 +27,6 @@ class DefaultModel extends SQLModel
         return $response;
     }
 
- dev_yulissa
-     /**
 
 
 
@@ -46,7 +44,7 @@ class DefaultModel extends SQLModel
     }
 }
 
-
+    /**
      * $params ['id' => 'value', 'name' => 'value']]
      * @return Array ['status', 'data']
      */
@@ -58,70 +56,4 @@ class DefaultModel extends SQLModel
         $query .= ' WHERE "id_product"=' . $params['id'];
         return $this->executeQuery($query);
     }
-
-     /**
-     * $params ['id' => 'value', 'name' => 'value']]
-     * @return Array ['status', 'data']
-     */
-    public function deleteProduct(array $params)
-    {
-        $query = 'DELETE FROM';
-        $query .= ' "public"."products"';
-        $query .= ' WHERE "id_product"=' . $params['id'];
-        return $this->executeQuery($query); 
-    }
-
-
-     /**
-     * get all users or one product
-     * @return Array ['status', 'data']
-     */
-    public function getUsersOrUser(String $schema, Array $columns, Array $condition, Array $order = [])
-    {
-        $this->setSchema($schema);
-        return $this->selectFromTable('users',$columns, $condition, $order);
-    }
-
-    /**
-     * insert product.
-     * @return Array ['status', 'data']
-     */
-    public function insertUser(String $schema, Array $values, Int $id)
-    {
-        $this->setSchema($schema);
-        $response = $this->insertIntoTable('users',$values, $id);
-        return $response;
-    }
-
-     /**
-     * $params ['id' => 'value', 'name' => 'value']]
-     * @return Array ['status', 'data']
-     */
-    public function updateUser(array $params)
-    {
-        $query = 'UPDATE';
-        $query .= ' "public"."users"';
-        $query .= ' SET "name"=' . "'" . $params['name']."'";
-        $query .= ' WHERE "id_user"=' . $params['id'];
-        return $this->executeQuery($query);
-    }
-
-    /**
-     * $params ['id' => 'value', 'name' => 'value']]
-     * @return Array ['status', 'data']
-     */
-    public function deleteUser(array $params)
-    {
-        $query = 'DELETE FROM';
-        $query .= ' "public"."users"';
-        $query .= ' WHERE "id_user"=' . $params['id'];
-        return $this->executeQuery($query); 
-    }
-}
-    
-
-
-   
-
-
 }
